c37f4750aec9d61605ca65bd51e5668fad8370f1
yasith8314 <yasith8314@gmail.com>
2026-04-11 00:51:58 +0530

    Convert page to a SPA with global context

--- shopping_list/src/main.jsx
+++ shopping_list/src/main.jsx
@@ -1,10 +1,52 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { StrictMode } from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+
 import './index.css'
 import App from './App.jsx'
+import { Last30Days, Next30Days, BestGamesOfTheYear, BestGamesOfAllTime } from './models/all_main_cards'
+import Library from './models/library'
+import { CartContext } from './models/cart.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />, // App is the global layout wrapper
+    children: [
+      { 
+        index: true, // This tells React Router: "Load this component by default when the user is at exactly '/'"
+        element: <BestGamesOfTheYear /> 
+      },
+      { path: "/last-30-days", element: <Last30Days />},
+      { path: "/next-30-days", element: <Next30Days />},
+      { path: "/best-games-of-all-time", element: <BestGamesOfAllTime /> },
+      { path: "/library", element: <Library /> }
+    ]
+  }
+])
+
+function Root() {
+  const [cart, setCart] = useState(
+    () => { return JSON.parse(localStorage.getItem("cart")) || []; }
+  );
+  
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+  
+  
+  return (
+    <CartContext.Provider value={{ cart, setCart }}>
+      <RouterProvider router={router} />
+    </CartContext.Provider>
+  );
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Root />
+  </StrictMode>
 )
